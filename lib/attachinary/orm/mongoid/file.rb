2ca7634ba1d2e33479661737df87ef09a7ae60a1
Siddhartha Gudipati <siddu.siddhartha@gmail.com>
2015-03-15 18:43:56 -0700

    Adding more fields

--- lib/attachinary/orm/mongoid/file.rb
+++ lib/attachinary/orm/mongoid/file.rb
@@ -5,12 +5,23 @@ module Attachinary
     include FileMixin
 
     field :public_id, type: String
+    field :format, type: String
     field :version, type: String
+    field :resource_type, type: String
+    field :bytes, type: Integer
+
     field :width, type: Integer
     field :height, type: Integer
-    field :format, type: String
+    field :url, type: String
+    field :secure_url, type: String
+    field :next_cursor, type: String
+
     field :resource_type, type: String
+    field :colors, type: Array
 
     embedded_in :attachinariable, polymorphic: true
   end
 end
+
+
+
